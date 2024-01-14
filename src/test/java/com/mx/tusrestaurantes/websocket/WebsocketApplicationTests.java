a87c8ceeef9c2ad65148ce2c90ec29f4fdb4bb0b
AruDev <zamauly@gmail.com>
2024-01-13 23:31:39 -0600

    initialize: first functional view, websocket doesnt connect

---
+++ src/test/java/com/mx/tusrestaurantes/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.tusrestaurantes.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
