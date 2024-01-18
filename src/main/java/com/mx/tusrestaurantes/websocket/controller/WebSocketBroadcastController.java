d4555a4e9ebbaa86fd9c0773d09065a79b449d8c
AruDev <zamauly@gmail.com>
2024-01-18 16:15:19 -0600

    release(inital): set inital params

---
+++ src/main/java/com/mx/tusrestaurantes/websocket/controller/WebSocketBroadcastController.java
@@ -0,0 +1,23 @@
+package com.mx.tusrestaurantes.websocket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mx.tusrestaurantes.websocket.dto.ChatMessage;
+ 
+@Controller
+public class WebSocketBroadcastController {
+ 
+    @GetMapping("/stomp-broadcast")
+    public String getWebSocketBroadcast() {
+        return "stomp-broadcast";
+    }
+    
+    @MessageMapping("/broadcast")
+    @SendTo("/topic/messages")
+    public ChatMessage send(ChatMessage chatMessage) throws Exception {
+        return new ChatMessage(chatMessage.getFrom(), chatMessage.getText(), "ALL");
+    }
+}
---
+++ src/main/java/com/mx/tusrestaurantes/websocket/exceptions/NumberOfConnectedClientsChannelInterceptor.java
@@ -0,0 +1,5 @@
+package com.mx.tusrestaurantes.websocket.exceptions;
+
+public class NumberOfConnectedClientsChannelInterceptor {
+
+}
